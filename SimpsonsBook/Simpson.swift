eab1d9c03eef3e9a814af80583391980415162c3
Eren Kaya <58259714+erenkaya0@users.noreply.github.com>
2022-07-29 12:57:16 +0300

    Initial commit

---
+++ SimpsonsBook/Simpson.swift
@@ -0,0 +1,23 @@
+//
+//  Simpson.swift
+//  SimpsonsBook
+//
+//  Created by eren on 1.02.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson{
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName : String, simpsonJob : String, simpsonImage : UIImage){
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+ 
+}
